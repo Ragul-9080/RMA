f5e7844ddb9d1a969ddfcb41ba0da5ddcd68b144
Ragul <102910276+Ragul-9080@users.noreply.github.com>
2025-03-27 19:20:44 +0530

    Update Header.tsx
    V2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from '../../taklogo (1).png'; // Importing the logo
+import logo from '../logo-white.png'; // Importing the logo
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-indigo-500 text-white p-4 text-center">
-
-      <h1 className="text-3xl font-bold inline-block uppercase text-white">TAKSHASHILA UNIVERSITY</h1>
-      <div className="mt-2">
-        <h2 className="text-lg text-white">School of Computer Science</h2>
-      </div>
+    <header className="bg-orange-600 text-white p-6 text-center shadow-lg transform transition-transform hover:scale-105 flex flex-col items-center">
+      <img src={logo} alt="Takshashila University Logo" className="h-16 mb-2" />
+      <h3 className="text-lg">School of Computer Science</h3>
     </header>
   );
 };
